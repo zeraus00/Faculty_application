8dbd6066c3654ed1788304a2c8155efd7917b8a1
zeraus00 <193580639+zeraus00@users.noreply.github.com>
2026-03-27 11:40:36 +0800

    added: frgt_email_verify.java

--- app/src/main/java/com/example/faculty_app/frgt_email_verify.java
+++ app/src/main/java/com/example/faculty_app/frgt_email_verify.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class e_verification extends AppCompatActivity {
+public class frgt_email_verify extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_everification);
+        setContentView(R.layout.activity_frgt_email_verify);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
