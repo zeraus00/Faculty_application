d90e7e1b774d32aa601b71732df123f2552010c2
steins0668 <archelausagaton@gmail.com>
2026-04-26 17:16:05 +0800

    feat: add mock build type and enable developer session for debugging

--- app/src/main/java/com/example/faculty_app/RVAUCMSFacultyApplication.java
+++ app/src/main/java/com/example/faculty_app/RVAUCMSFacultyApplication.java
@@ -1,8 +1,10 @@
 package com.example.faculty_app;
 
 import android.app.Application;
+import android.util.Log;
 
 
+import com.auth0.android.jwt.BuildConfig;
 import com.example.faculty_app.core.api.rvaucms.RvaucMsService;
 import com.example.faculty_app.core.auth.AuthInterceptor;
 import com.example.faculty_app.core.auth.SessionManager;
@@ -23,5 +25,17 @@ public class RVAUCMSFacultyApplication extends Application {
                 },
                 new TokenAuthenticator()
         );
+
+        if (BuildConfig.DEBUG) enableDevSession();
+    }
+    private void enableDevSession() {
+        Log.d("DEV_SESSION_ENABLED", "Developer session enabled. Authentication is mocked.");
+        SessionManager session = SessionManager.getInstance();
+
+        session.setEmail("dev@local");
+        session.setAccessToken("dev-token");
+        session.setRememberMe(true);
+
+        //  optional mock payload
     }
 }
