06114025cd805dbcc5684277df3e51f3b928ba10
steins0668 <archelausagaton@gmail.com>
2026-04-27 18:35:30 +0800

    refactor base callback

--- app/src/main/java/com/example/faculty_app/shared/BaseCallback.java
+++ app/src/main/java/com/example/faculty_app/shared/BaseCallback.java
@@ -1,7 +1,5 @@
 package com.example.faculty_app.shared;
 
-public interface BaseCallback<T> {
-    void onSuccess(T dto);
-
-    void onFail(String message);
+public interface BaseCallback<TResult> {
+    void onResult(BaseResult<TResult> result);
 }
